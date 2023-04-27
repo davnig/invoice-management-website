c80679a36d36d8d4d0297ac1c25df9c932465983
davnig <daddets97@gmail.com>
2023-04-27 17:42:22 +0200

    chore(be): rename entity

--- backend/src/main/java/com/davnig/invoicemanagementapi/model/entity/BillingItem.java
+++ backend/src/main/java/com/davnig/invoicemanagementapi/model/entity/BillingItem.java
@@ -1,20 +1,19 @@
 package com.davnig.invoicemanagementapi.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class InvoiceItem {
+public class BillingItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @ManyToOne
+    private BillingItemType type;
     private String description;
     private int price;
     private int quantity;

b9abd0483970644876d061ace0cb644b00f54b0f
davnig <daddets97@gmail.com>
2023-04-27 12:09:31 +0200

    chore: create backend and frontend folders

---
+++ backend/src/main/java/com/davnig/invoicemanagementapi/InvoiceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.davnig.invoicemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManagementApiApplication.class, args);
+	}
+
+}
